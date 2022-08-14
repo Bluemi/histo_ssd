f25c16f7e75099184eece9621df662c968c588cd
Bluemi <bruno.schilling@protonmail.ch>
2022-08-14 11:33:56 +0200

    minor changes

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ case "$1" in
 		det -m "https://dt1.f4.htw-berlin.de:8443" -u bschilling experiment create --follow-first-trial ./configs/test_tiny.yaml ./src
 		;;
 	lc|localcluster)
-		det -m "http://localhost:8080" -u admin experiment create --follow-first-trial ./configs/test_tiny.yaml ./src
+		det -m "http://localhost:8080" -u admin experiment create ./configs/test_tiny.yaml ./src
 		;;
 	d|det)
 		det -m "https://dt1.f4.htw-berlin.de:8443" -u bschilling "$@"
