fix banana ds normalization

src/datasets/banana_dataset.py
@@ -14,11 +14,11 @@ def read_data_bananas(data_dir: str, is_train=True, verbose=True):
     csv_data = csv_data.set_index('img_name')
     images, targets = [], []
     for img_name, target in csv_data.iterrows():
-        images.append(
-            torchvision.io.read_image(
-                os.path.join(data_dir, 'bananas_train' if is_train else 'bananas_val', 'images', f'{img_name}')
-            )
+        image = torchvision.io.read_image(
+            os.path.join(data_dir, 'bananas_train' if is_train else 'bananas_val', 'images', f'{img_name}')
         )
+        image = image.float() / 255.0  # normalize images
+        images.append(image)
         # Here `target` contains (class, upper-left x, upper-left y, lower-right x, lower-right y),
         # where all the images have the same banana class (index 0)
         targets.append(list(target))
@@ -31,7 +31,6 @@ class BananasDataset(torch.utils.data.Dataset):
         self.features, self.labels = read_data_bananas(data_dir, is_train, verbose=verbose)
         if verbose:
             print('read ' + str(len(self.features)) + (f' training examples' if is_train else f' validation examples'))
-        self.features /= 255.0  # normalize images to be between 0 and 1
 
     def __getitem__(self, idx):
         return {