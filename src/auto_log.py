8ad626826a3bc1fdda82e4bdec8f56311fcbadff
Bluemi <bruno.schilling@protonmail.ch>
2022-08-15 12:47:13 +0200

    improved auto_log

--- src/auto_log.py
+++ src/auto_log.py
@@ -6,21 +6,22 @@ import sys
 
 def init_auto_log(name: str):
     # path
-    logger = logging.getLogger(__name__)
     experiment = 'experiment{:0>5}'.format(os.environ.get('DET_EXPERIMENT_ID', '_unknown'))
     log_dir = Path('/logs') / name / experiment
     os.makedirs(log_dir, exist_ok=True)
 
     # stdout / stderr
-    stdout_path = log_dir / 'stdout'
+    stdout_path = log_dir / 'out'
     sys.stdout = open(stdout_path, 'w')
-    stderr_path = log_dir / 'stderr'
+    stderr_path = log_dir / 'err'
     sys.stderr = open(stderr_path, 'w')
-    log_path = log_dir / 'stacktrace'
 
     # exception handler
+    log_path = log_dir / 'trace'
     outfile = open(log_path, 'w')
     handler = logging.StreamHandler(stream=outfile)
+
+    logger = logging.getLogger(__name__)
     logger.addHandler(handler)
 
     def _handle_exception(exc_type, exc_value, exc_traceback):
