aa81f5b83c3ce1c3a88ee77763069a2f182bb7d5
Bluemi <bruno.schilling@protonmail.ch>
2022-08-16 12:08:39 +0200

    removed local test

--- run.sh
+++ run.sh
@@ -4,9 +4,6 @@ case "$1" in
 	li|lizard)
 		PYTHONPATH=./src python3 ./tests/test_lizard.py
 		;;
-	l|local)
-		PYTHONPATH=./src python3 ./src/local_test.py
-		;;
 	c|cluster)
 		det -m "https://dt1.f4.htw-berlin.de:8443" -u bschilling experiment create ./configs/test_tiny.yaml ./src
 		;;
