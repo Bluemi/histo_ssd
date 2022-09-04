9cc07adf1528e88add9ef338c95d6dcc88034399
Bluemi <bruno.schilling@protonmail.ch>
2022-09-04 20:24:27 +0200

    worked on scale up config

--- run.sh
+++ run.sh
@@ -28,41 +28,45 @@ case "$1" in
 		fi
 		patches=(
 			# p/cluster/pepper.yaml
-			p/cluster/pepper_few.yaml
-			# p/cluster/dt.yaml
+			# p/cluster/pepper_few.yaml
+			p/cluster/dt.yaml
 
 			p/dataset/lizard.yaml
 			p/dataset/ignore_rare_classes.yaml
-			# p/dataset/scale_up.yaml
+			p/dataset/scale_up.yaml
 
-			# p/model/vgg16.yaml
-			p/model/vgg16early.yaml
+			p/model/vgg16.yaml
+			# p/model/vgg16early.yaml
 
 			# p/augmentation/test_rotate_flip.yaml
 			# p/hparams/lr_narrow.yaml
-			p/hparams/lr.yaml
+			# p/hparams/lr.yaml
 			# p/hparams/sizes.yaml
-			p/hparams/sizes_small.yaml
+			# p/hparams/sizes_small.yaml
 			# p/hparams/negative_ratio_narrow.yaml
 			# p/hparams/image_stride.yaml
 			# p/hparams/smoothl1.yaml
 			# p/hparams/nms_threshold.yaml
 			# p/hparams/bbox_loss_scale.yaml
 
-			p/metrics/loss.yaml
-			# p/metrics/map.yaml
-			# p/metrics/write_predictions.yaml
+			# p/metrics/loss.yaml
+			p/metrics/map.yaml
+			p/metrics/write_predictions.yaml
 
-			# p/checkpointing/save_checkpoints.yaml
-			# p/checkpointing/load_checkpoint.yaml
+			p/checkpointing/save_checkpoints.yaml
+			p/checkpointing/load_checkpoint.yaml
 
 			# p/searcher/adaptive_asha.yaml
-			p/searcher/adaptive_asha_few.yaml
+			# p/searcher/adaptive_asha_few.yaml
 			# p/searcher/random.yaml
 
 			p/profiling/clock.yaml
 			)
-		python3 ./utils/start_experiment.py -v $dry "./configs/base_config.yaml" "${patches[@]}"
+
+		base_config="./configs/base_config.yaml"
+		base_config="./configs/scale_up_config.yaml"
+
+		python3 ./utils/start_experiment.py -v $dry "$base_config" "${patches[@]}"
 		;;
 	o|orig)
 		PYTHONPATH=./src python3 ./tests/test_orig.py
