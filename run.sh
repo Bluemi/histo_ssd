583ed9363f913ae62e85b7dcd758770f1ae5a728
Bluemi <bruno.schilling@protonmail.ch>
2022-09-05 09:46:48 +0200

    prepared vgg16-e experiment

--- run.sh
+++ run.sh
@@ -27,22 +27,22 @@ case "$1" in
 			shift
 		fi
 		patches=(
-			# p/cluster/pepper.yaml
+			p/cluster/pepper.yaml
 			# p/cluster/pepper_few.yaml
-			p/cluster/dt.yaml
+			# p/cluster/dt.yaml
 
 			p/dataset/lizard.yaml
 			p/dataset/ignore_rare_classes.yaml
-			p/dataset/scale_up.yaml
+			# p/dataset/scale_up.yaml
 
-			p/model/vgg16.yaml
-			# p/model/vgg16early.yaml
+			# p/model/vgg16.yaml
+			p/model/vgg16early.yaml
 
 			# p/augmentation/test_rotate_flip.yaml
 			# p/hparams/lr_narrow.yaml
-			# p/hparams/lr.yaml
+			p/hparams/lr.yaml
 			# p/hparams/sizes.yaml
-			# p/hparams/sizes_small.yaml
+			p/hparams/sizes_small.yaml
 			# p/hparams/negative_ratio_narrow.yaml
 			# p/hparams/image_stride.yaml
 			# p/hparams/smoothl1.yaml
@@ -54,17 +54,17 @@ case "$1" in
 			p/metrics/write_predictions.yaml
 
 			p/checkpointing/save_checkpoints.yaml
-			p/checkpointing/load_checkpoint.yaml
+			# p/checkpointing/load_checkpoint.yaml
 
 			# p/searcher/adaptive_asha.yaml
 			# p/searcher/adaptive_asha_few.yaml
 			# p/searcher/random.yaml
 
-			# p/profiling/clock.yaml
+			p/profiling/clock.yaml
 		)
 
 		base_config="./configs/base_config.yaml"
-		base_config="./configs/scale_up_config.yaml"
+		# base_config="./configs/scale_up_config.yaml"
 
 		python3 ./utils/start_experiment.py -v $dry "$base_config" "${patches[@]}"
 		;;
