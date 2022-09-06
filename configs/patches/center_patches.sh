51941fbb5b6f804ca236eb1e45d2fc2321435c9b
Bluemi <bruno.schilling@protonmail.ch>
2022-09-06 17:12:53 +0200

    new patch configuration system :3

---
+++ configs/patches/center_patches.sh
@@ -0,0 +1,42 @@
+base_config="./configs/center_config.yaml"
+patches=(
+	# p/cluster/pepper.yaml
+	# p/cluster/pepper_few.yaml
+	p/cluster/dt.yaml
+
+	p/dataset/lizard.yaml
+	p/dataset/ignore_rare_classes.yaml
+	# p/dataset/scale_up.yaml
+
+	p/model/vgg16.yaml
+	# p/model/vgg16early.yaml
+
+	# p/augmentation/test_rotate_flip.yaml
+	# p/hparams/lr_narrow.yaml
+	# p/hparams/lr.yaml
+	# p/hparams/sizes.yaml
+	# p/hparams/sizes_small.yaml
+	# p/hparams/sizes_med.yaml
+	# p/hparams/negative_ratio_narrow.yaml
+	# p/hparams/image_stride.yaml
+	# p/hparams/smoothl1.yaml
+	# p/hparams/nms_threshold.yaml
+	# p/hparams/bbox_loss_scale.yaml
+	# p/hparams/iou_match_threshold.yaml
+
+	p/metrics/loss.yaml
+	# p/metrics/map.yaml
+	# p/metrics/write_predictions.yaml
+	# p/metrics/write_few_predictions.yaml
+
+	# p/checkpointing/save_checkpoints.yaml
+	# p/checkpointing/load_checkpoint.yaml
+	# p/checkpointing/load_checkpoint_scaleup.yaml
+	# p/checkpointing/load_checkpoint_early.yaml
+
+	# p/searcher/adaptive_asha.yaml
+	# p/searcher/adaptive_asha_few.yaml
+	# p/searcher/random.yaml
+
+	# p/profiling/clock.yaml
+)
--- run.sh
+++ run.sh
@@ -26,52 +26,25 @@ case "$1" in
 			dry='--dry'
 			shift
 		fi
-		patches=(
-			# p/cluster/pepper.yaml
-			# p/cluster/pepper_few.yaml
-			p/cluster/dt.yaml
 
-			p/dataset/lizard.yaml
-			p/dataset/ignore_rare_classes.yaml
-			# p/dataset/scale_up.yaml
-
-			p/model/vgg16.yaml
-			# p/model/vgg16early.yaml
-
-			# p/augmentation/test_rotate_flip.yaml
-			# p/hparams/lr_narrow.yaml
-			# p/hparams/lr.yaml
-			# p/hparams/sizes.yaml
-			# p/hparams/sizes_small.yaml
-			# p/hparams/sizes_med.yaml
-			# p/hparams/negative_ratio_narrow.yaml
-			# p/hparams/image_stride.yaml
-			# p/hparams/smoothl1.yaml
-			# p/hparams/nms_threshold.yaml
-			# p/hparams/bbox_loss_scale.yaml
-			# p/hparams/iou_match_threshold.yaml
-
-			p/metrics/loss.yaml
-			# p/metrics/map.yaml
-			# p/metrics/write_predictions.yaml
-			p/metrics/write_few_predictions.yaml
-
-			# p/checkpointing/save_checkpoints.yaml
-			# p/checkpointing/load_checkpoint.yaml
-			# p/checkpointing/load_checkpoint_scaleup.yaml
-			# p/checkpointing/load_checkpoint_early.yaml
-
-			# p/searcher/adaptive_asha.yaml
-			# p/searcher/adaptive_asha_few.yaml
-			# p/searcher/random.yaml
-
-			p/profiling/clock.yaml
-		)
-
-		# base_config="./configs/standard_config.yaml"
-		# base_config="./configs/vgg16_early_config.yaml"
-		# base_config="./configs/scale_up_config.yaml"
-		base_config="./configs/center_config.yaml"
+		case "$1" in
+			d|default)
+				source ./p/default_patches.sh
+				;;
+			e|early)
+				source ./p/early_patches.sh
+				;;
+			s|scaleup)
+				source ./p/scaleup_patches.sh
+				;;
+			c|center)
+				source ./p/center_patches.sh
+				;;
+			*)
+				echo "ERROR: unknown patch configuration: $1"
+				exit 1
+				;;
+		esac
 
 		python3 ./utils/start_experiment.py -v $dry "$base_config" "${patches[@]}"
 		;;
