6ac0c3ad875fc6c1f0bd7b9986569405f0577a32
Bluemi <bruno.schilling@protonmail.ch>
2022-09-08 21:20:15 +0200

    new iou_match_threshold config; removed augmentation from description

--- configs/patches/standard_patches.sh
+++ configs/patches/standard_patches.sh
@@ -1,9 +1,9 @@
 base_config="./configs/standard_config.yaml"
 patches=(
 	# p/cluster/pepper.yaml
-	# p/cluster/pepper_full.yaml
+	p/cluster/pepper_full.yaml
 	# p/cluster/pepper_few.yaml
-	p/cluster/dt.yaml
+	# p/cluster/dt.yaml
 
 	p/dataset/lizard.yaml
 	p/dataset/ignore_rare_classes.yaml
@@ -21,8 +21,8 @@ patches=(
 	# p/hparams/negative_ratio_narrow.yaml
 	# p/hparams/image_stride.yaml
 	# p/hparams/smoothl1.yaml
-	# p/hparams/nms_threshold.yaml
-	# p/hparams/iou_match_threshold.yaml
+	p/hparams/nms_threshold.yaml
+	p/hparams/iou_match_threshold.yaml
 	# p/hparams/bbox_loss_scale.yaml
 
 	p/metrics/loss.yaml
@@ -37,7 +37,7 @@ patches=(
 
 	# p/searcher/adaptive_asha.yaml
 	# p/searcher/adaptive_asha_few.yaml
-	# p/searcher/random.yaml
+	p/searcher/random.yaml
 
 	p/profiling/clock.yaml
 )
--- utils/start_experiment.py
+++ utils/start_experiment.py
@@ -46,7 +46,6 @@ def main():
 
     variables = {
         'description': {
-            'augmentation': 'no augmentation',
             'action': 'test',
             'irc': '',
         }
