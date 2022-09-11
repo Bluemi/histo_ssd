c201d4b5c6924442a66d1e8e3361f37faff1563d
Bluemi <bruno.schilling@protonmail.ch>
2022-09-11 15:22:20 +0200

    updated scale up config with hparams from std config

--- configs/patches/scaleup_patches.sh
+++ configs/patches/scaleup_patches.sh
@@ -1,8 +1,9 @@
 base_config="./configs/scale_up_config.yaml"
 patches=(
-	p/cluster/pepper.yaml
+	# p/cluster/pepper.yaml
+	# p/cluster/pepper_full.yaml
 	# p/cluster/pepper_few.yaml
-	# p/cluster/dt.yaml
+	p/cluster/dt.yaml
 
 	p/dataset/lizard.yaml
 	p/dataset/ignore_rare_classes.yaml
@@ -14,7 +15,7 @@ patches=(
 	# p/augmentation/test_rotate_flip.yaml
 	# p/hparams/lr_narrow.yaml
 	# p/hparams/lr.yaml
-	# p/hparams/sizes.yaml
+	p/hparams/sizes.yaml
 	# p/hparams/sizes_small.yaml
 	# p/hparams/sizes_med.yaml
 	# p/hparams/negative_ratio_narrow.yaml
@@ -22,13 +23,13 @@ patches=(
 	# p/hparams/smoothl1.yaml
 	# p/hparams/nms_threshold.yaml
 	# p/hparams/bbox_loss_scale.yaml
-	p/hparams/iou_match_threshold.yaml
+	# p/hparams/iou_match_threshold.yaml
 
-	p/metrics/loss.yaml
+	# p/metrics/loss.yaml
 	# p/metrics/map.yaml
-	# p/metrics/f1.yaml
-	# p/metrics/write_predictions.yaml
-	p/metrics/write_few_predictions.yaml
+	p/metrics/f1.yaml
+	p/metrics/write_predictions.yaml
+	# p/metrics/write_few_predictions.yaml
 
 	# p/checkpointing/save_checkpoints.yaml
 	# p/checkpointing/load_checkpoint.yaml
